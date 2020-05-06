5a6a9df37272315dc21f2c5740063f7fdf6143f1
Kalin Tsenkov <kalintsenkov3@gmail.com>
2020-05-06 12:05:16 +0300

    Add WebAssemblyHostBuilder extensions

--- src/Web/SheryLady.Web/Client/Program.cs
+++ src/Web/SheryLady.Web/Client/Program.cs
@@ -1,31 +1,19 @@
 namespace SheryLady.Web.Client
 {
-    using System;
-    using System.Net.Http;
     using System.Threading.Tasks;
 
-    using Microsoft.AspNetCore.Components.Authorization;
     using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-    using Microsoft.Extensions.DependencyInjection;
 
     using Infrastructure;
 
     public class Program
     {
-        public static async Task Main(string[] args)
-        {
-            var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("app");
-            
-            builder.Services.AddAuthorizationCore();
-            
-            builder.Services.AddScoped<TokenAuthenticationStateProvider>();
-            builder.Services.AddScoped<AuthenticationStateProvider>(provider => provider
-                .GetRequiredService<TokenAuthenticationStateProvider>());
-            builder.Services.AddTransient(sp => 
-                new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
-            await builder.Build().RunAsync();
-        }
+        public static async Task Main(string[] args) 
+            => await WebAssemblyHostBuilder
+                .CreateDefault(args)
+                .AddAppRootComponents()
+                .AddClientServices()
+                .Build()
+                .RunAsync();
     }
 }
