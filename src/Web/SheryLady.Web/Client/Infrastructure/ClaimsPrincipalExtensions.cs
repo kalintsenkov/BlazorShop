795e73b4d64a8a7458f42cd60310eebd3b29b412
Kalin Tsenkov <kalintsenkov3@gmail.com>
2020-05-06 20:54:37 +0300

    Add ClaimsPrincipal extensions

---
+++ src/Web/SheryLady.Web/Client/Infrastructure/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace SheryLady.Web.Client.Infrastructure
+{
+    using System.Security.Claims;
+
+    using Common;
+
+    public static class ClaimsPrincipalExtensions
+    {
+        public static string GetId(this ClaimsPrincipal claimsPrincipal)
+            => claimsPrincipal.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+        public static bool IsAdministrator(this ClaimsPrincipal claimsPrincipal)
+            => claimsPrincipal.IsInRole(GlobalConstants.AdminRoleName);
+    }
+}
