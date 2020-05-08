c56776fb7b658d334856209c42c7cc41926ef138
Kalin Tsenkov <kalintsenkov3@gmail.com>
2020-05-08 19:26:50 +0300

    Add .gitattributes file

--- src/SheryLady.Common/GlobalConstants.cs
+++ src/SheryLady.Common/GlobalConstants.cs
@@ -2,6 +2,8 @@
 {
     public class GlobalConstants
     {
+        public const string SystemName = "Shery Lady";
+
         public const string AdminRoleName = "Admin";
         public const string AdminEmail = "admin@sherylady.bg";
         public const string AdminPassword = "admin123456";
