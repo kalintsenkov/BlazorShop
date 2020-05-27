894056448e3870070d4fd6aa2d1015b059fa3ff9
Kalin Tsenkov <kalintsenkov3@gmail.com>
2020-05-27 20:26:11 +0300

    Add Components folder in BlazorShop.Web/Client project

---
+++ src/Web/BlazorShop.Web/Client/Components/InputSelectNumber.cs
@@ -0,0 +1,28 @@
+﻿namespace BlazorShop.Web.Client.Components
+{
+    using Microsoft.AspNetCore.Components.Forms;
+
+    public class InputSelectNumber<TValue> : InputSelect<TValue>
+    {
+        protected override bool TryParseValueFromString(string value, out TValue result, out string validationErrorMessage)
+        {
+            if (typeof(TValue) != typeof(int))
+            {
+                return base.TryParseValueFromString(value, out result, out validationErrorMessage);
+            }
+
+            if (int.TryParse(value, out var resultInt))
+            {
+                result = (TValue)(object)resultInt;
+                validationErrorMessage = null;
+                return true;
+            }
+            else
+            {
+                result = default;
+                validationErrorMessage = "The chosen value is not a valid number.";
+                return false;
+            }
+        }
+    }
+}
