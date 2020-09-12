2bd720732bf6fa1f6c281d8cc14928a5f4d1d014
Kalin Tsenkov <kalintsenkov3@gmail.com>
2020-09-12 13:36:35 +0300

    Add new categories and fix categories sidebar

--- src/BlazorShop.Web/Server/Infrastructure/Extensions/ApplicationBuilderExtensions.cs
+++ src/BlazorShop.Web/Server/Infrastructure/Extensions/ApplicationBuilderExtensions.cs
@@ -82,9 +82,9 @@
 
             var categories = new List<Category>
             {
-                new Category { Name = "Men" },
-                new Category { Name = "Women" },
-                new Category { Name = "Kids" }
+                new Category { Name = "Fashion" },
+                new Category { Name = "Electronics" },
+                new Category { Name = "Books, Movies & Music" }
             };
 
             await dbContext.Categories.AddRangeAsync(categories);
@@ -116,10 +116,37 @@
                 },
                 new Product
                 {
-                    Name = "Boardwalk Shorts",
-                    Description = "The Boardwalk Shorts are soft and comfortable making walking for hours a breeze.",
-                    ImageSource = "https://photos.cdn-outlet.com/photos/options/8172804-48426-1A-zoomin.jpg",
-                    Price = 37.99m,
+                    Name = "Beats by Dr. Dre Solo3 Wireless On-Ear Headphones",
+                    Description = "Beats Solo 3 is easy to set up, simply power on and hold near your iPhone, Apple Watch, iPad and Mac. Bluetooth technology makes it easy to instantly enjoy music from your Apple devices.",
+                    ImageSource = "https://i.ebayimg.com/images/g/OkQAAOSwFdRe18Yq/s-l1600.jpg",
+                    Price = 109.60m,
+                    Quantity = 10,
+                    CategoryId = 2
+                },
+                new Product
+                {
+                    Name = "Apple Watch Series 5",
+                    Description = "Keep your workouts fresh and enjoyable by streaming your favorite audio with the Apple Watch Series 5. The Series 5 has access to the Apple Music library, letting you stream your favorite albums, podcasts, and audiobooks.",
+                    ImageSource = "https://static.plasico.bg/thumbs/12/mwvf2bsa.jpg",
+                    Price = 366.98m,
+                    Quantity = 10,
+                    CategoryId = 2
+                },
+                new Product
+                {
+                    Name = "Code Complete - Second Edition",
+                    Description = "CODE COMPLETE has been helping developers write better software for more than a decade.",
+                    ImageSource = "https://i.ebayimg.com/images/g/55gAAOSwIqtfW63B/s-l500.png",
+                    Price = 35.99m,
+                    Quantity = 10,
+                    CategoryId = 3
+                },
+                new Product
+                {
+                    Name = "Rick and Morty: Season 3 - Blu-Ray",
+                    Description = "An animated series that follows the exploits of a super scientist and his not-so-bright grandson.",
+                    ImageSource = "https://www.augoods.com.au/assets/full/DVD-LN-9322225226746.jpg?20200703062244",
+                    Price = 28.95m,
                     Quantity = 10,
                     CategoryId = 3
                 }
